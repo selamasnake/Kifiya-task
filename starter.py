b3976b9a5c490514dc148010ee3640503c9b0452
selamasnake <selamsasnake@gmail.com>
2024-12-08 01:32:39 +0300

    loaded dataset

--- starter.py
+++ starter.py
@@ -1,1 +1,8 @@
-print("test repo setup")
+import pandas as pd
+import numpy as np
+import matplotlib.pylab as plt
+import seaborn as sns
+plt.style.use('ggplot')
+# pd.set_option('max_columns', 200)
+
+df = pd.read_csv('/Users/hazel/Downloads/data/benin-malanville.csv')
